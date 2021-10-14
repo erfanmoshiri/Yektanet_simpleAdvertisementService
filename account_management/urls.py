6c150bb20cbe407c5c7e923814a1bce0084828ab
Erfan Moshiri <erfanmoshiri99@gmail.com>
2021-10-14 11:05:01 +0330

    ViewSet added to user app views

--- account_management/urls.py
+++ account_management/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from account_management.views import Register_Users, login_user
+from account_management.views import Register_Users, login_user, UserViewSet
 
-urlpatterns = [
-    path('signup', Register_Users.as_view()),
-    path('login', login_user.as_view()),
+# urlpatterns = [
+#     path('signup', Register_Users.as_view()),
+#     path('login', login_user.as_view()),
+#
+# ]
 
-]
+# from views import UserViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+urlpatterns = router.urls
